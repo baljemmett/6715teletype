906059e1fec700c284344ee67d970e56a01ec1f5
Ben A L Jemmett <ben@jemmett.me.uk>
2016-01-16 17:56:58 +0000

    Implement DTR handshake line.
    This signals to the other end that our reception buffer is pretty full and should be given a chance to catch up; it's implemented with a simple high/low-watermark.  DSR is currently ignored...

--- uart.c
+++ uart.c
@@ -1,8 +1,10 @@
 #include <xc.h>
 #include "uart.h"
 
-#define TX_BUFFER_SIZE 16
-#define RX_BUFFER_SIZE 128
+#define TX_BUFFER_SIZE 8
+#define RX_BUFFER_SIZE 64
+#define RX_BUFFER_HIGHWATER 32
+#define RX_BUFFER_LOWWATER  8
 
 #if TX_BUFFER_SIZE > 0
 static volatile char achTxBuffer[TX_BUFFER_SIZE];
@@ -13,6 +15,19 @@ static volatile unsigned char idxTxRead = 0, idxTxWrite = 0;
 static volatile char achRxBuffer[RX_BUFFER_SIZE];
 static volatile unsigned char idxRxRead = 0, idxRxWrite = 0;
 #endif
+
+#define nDTR LATA3
+#define nDSR PORTA2
+
+void uart_block_sender(void)
+{
+    nDTR = 1;
+}
+
+void uart_unblock_sender(void)
+{
+    nDTR = 0;
+}
 
 void uart_init(void)
 {
@@ -30,11 +45,16 @@ void uart_init(void)
     
     TRISC6 = 0;
     
+    TRISA3 = 0; // DTR output
+    TRISA2 = 1; // DSR input
+
 #if RX_BUFFER_SIZE > 0
     RCIE = 1;
 #endif
     
     PEIE = 1;
+
+    uart_unblock_sender();
 }
 
 void uart_tx_isr(void)
@@ -49,12 +69,28 @@ void uart_tx_isr(void)
 #endif
 }
 
+#if RX_BUFFER_SIZE > 0
+static unsigned char uart_rx_buffer_remaining(void)
+{
+    return (idxRxWrite > idxRxRead) ?
+           (idxRxWrite - idxRxRead) :
+           (RX_BUFFER_SIZE - idxRxRead + idxRxWrite);
+}
+#endif
+
 void uart_rx_isr(void)
 {
 #if RX_BUFFER_SIZE > 0
     achRxBuffer[idxRxWrite++] = RCREG;
     if (idxRxWrite == RX_BUFFER_SIZE)
+    {
         idxRxWrite = 0;
+    }
+    
+    if (uart_rx_buffer_remaining() < RX_BUFFER_HIGHWATER)
+    {
+        uart_block_sender();
+    }
 #endif   
 }
 
@@ -91,6 +127,11 @@ char uart_get_rx_byte(void)
     if (idxRxRead == RX_BUFFER_SIZE)
         idxRxRead = 0;
     
+    if (uart_rx_buffer_remaining() > RX_BUFFER_LOWWATER)
+    {
+        uart_unblock_sender();
+    }
+
     return ch;
 #else
     if (! RCIF)
--- uart.h
+++ uart.h
@@ -16,6 +16,8 @@ extern "C" {
     extern void uart_tx_isr(void);
     extern void uart_rx_isr(void);
     extern char uart_get_rx_byte(void);
+    extern void uart_block_sender(void);
+    extern void uart_unblock_sender(void);
 
 
 #ifdef	__cplusplus
