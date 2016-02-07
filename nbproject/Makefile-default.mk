eb25a79992652aef2ef8196706fad2bc506cfd12
Ben A L Jemmett <ben@jemmett.me.uk>
2016-02-07 00:12:28 +0000

    Add new source files to project, oops.

--- nbproject/Makefile-default.mk
+++ nbproject/Makefile-default.mk
@@ -45,17 +45,17 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
 # Source Files Quoted if spaced
-SOURCEFILES_QUOTED_IF_SPACED=main.c keyboard.c uart.c terminal.c
+SOURCEFILES_QUOTED_IF_SPACED=main.c keyboard.c uart.c terminal.c timers.c
 
 # Object Files Quoted if spaced
-OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/main.p1 ${OBJECTDIR}/keyboard.p1 ${OBJECTDIR}/uart.p1 ${OBJECTDIR}/terminal.p1
-POSSIBLE_DEPFILES=${OBJECTDIR}/main.p1.d ${OBJECTDIR}/keyboard.p1.d ${OBJECTDIR}/uart.p1.d ${OBJECTDIR}/terminal.p1.d
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/main.p1 ${OBJECTDIR}/keyboard.p1 ${OBJECTDIR}/uart.p1 ${OBJECTDIR}/terminal.p1 ${OBJECTDIR}/timers.p1
+POSSIBLE_DEPFILES=${OBJECTDIR}/main.p1.d ${OBJECTDIR}/keyboard.p1.d ${OBJECTDIR}/uart.p1.d ${OBJECTDIR}/terminal.p1.d ${OBJECTDIR}/timers.p1.d
 
 # Object Files
-OBJECTFILES=${OBJECTDIR}/main.p1 ${OBJECTDIR}/keyboard.p1 ${OBJECTDIR}/uart.p1 ${OBJECTDIR}/terminal.p1
+OBJECTFILES=${OBJECTDIR}/main.p1 ${OBJECTDIR}/keyboard.p1 ${OBJECTDIR}/uart.p1 ${OBJECTDIR}/terminal.p1 ${OBJECTDIR}/timers.p1
 
 # Source Files
-SOURCEFILES=main.c keyboard.c uart.c terminal.c
+SOURCEFILES=main.c keyboard.c uart.c terminal.c timers.c
 
 
 CFLAGS=
@@ -113,6 +113,14 @@ ${OBJECTDIR}/terminal.p1: terminal.c  nbproject/Makefile-${CND_CONF}.mk
 	@-${MV} ${OBJECTDIR}/terminal.d ${OBJECTDIR}/terminal.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/terminal.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
+${OBJECTDIR}/timers.p1: timers.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/timers.p1.d 
+	@${RM} ${OBJECTDIR}/timers.p1 
+	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  -D__DEBUG=1 --debugger=pickit3  --double=24 --float=24 --opt=default,+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 --warn=0 --asmlist --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-osccal,-resetbits,-download,-stackcall,+clib --output=-mcof,+elf:multilocs --stack=compiled:auto:auto "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/timers.p1  timers.c 
+	@-${MV} ${OBJECTDIR}/timers.d ${OBJECTDIR}/timers.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/timers.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	
 else
 ${OBJECTDIR}/main.p1: main.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} "${OBJECTDIR}" 
@@ -145,6 +153,14 @@ ${OBJECTDIR}/terminal.p1: terminal.c  nbproject/Makefile-${CND_CONF}.mk
 	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  --double=24 --float=24 --opt=default,+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 --warn=0 --asmlist --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-osccal,-resetbits,-download,-stackcall,+clib --output=-mcof,+elf:multilocs --stack=compiled:auto:auto "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/terminal.p1  terminal.c 
 	@-${MV} ${OBJECTDIR}/terminal.d ${OBJECTDIR}/terminal.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/terminal.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	
+${OBJECTDIR}/timers.p1: timers.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/timers.p1.d 
+	@${RM} ${OBJECTDIR}/timers.p1 
+	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  --double=24 --float=24 --opt=default,+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 --warn=0 --asmlist --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-osccal,-resetbits,-download,-stackcall,+clib --output=-mcof,+elf:multilocs --stack=compiled:auto:auto "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/timers.p1  timers.c 
+	@-${MV} ${OBJECTDIR}/timers.d ${OBJECTDIR}/timers.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/timers.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
 endif
 
